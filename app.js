3d7cb0a619485d1f668c39976fe63067ee1c08b0
la5chine <khachinechaker@gmail.com>
2019-06-22 17:10:34 +0100

    make the userlist page

--- app.js
+++ app.js
@@ -25,6 +25,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
